ee71c79a074e2926c887198650134e6f687ca912
Adam Gold <47038242+acgold@users.noreply.github.com>
2022-05-25 08:57:58 -0400

    combining files

--- drift_correction.py
+++ drift_correction.py
@@ -1,11 +1,10 @@
 import pandas as pd
 import numpy as np
 import datetime
-from atm_pressure import *
-from sqlalchemy import create_engine
 import warnings
 import os
 import statsmodels.api as sm
+from sqlalchemy import create_engine
 
 #######################
 # Utility functions   #
@@ -50,6 +49,48 @@ def qa_qc_flag(x, delta_wd_per_minute = 0.1):
     x.drop(columns = ["lag_sensor_water_depth", "lag_duration_minutes", "lag_wd_per_minute"], inplace = True)
     
     return x
+
+
+def match_measurements_to_survey(measurements, surveys):
+    sites = measurements["sensor_ID"].unique()
+    survey_sites = surveys["sensor_ID"].unique()
+    
+    matching_sites = list(set(sites) & set(survey_sites))
+    missing_sites = list(set(sites).difference(survey_sites))
+    
+    if len(missing_sites) > 0:
+        warnings.warn(message = str("Missing survey data for: " + ''.join(missing_sites) + ". The site(s) will not be processed."))    
+    
+    matched_measurements = pd.DataFrame()
+    
+    for selected_site in matching_sites:
+        selected_measurements = measurements.query("sensor_ID == @selected_site").copy()
+        
+        selected_survey = surveys.query("sensor_ID == @selected_site")
+        
+        if selected_survey.empty:
+            warnings.warn("There are no survey data for: " + selected_site)
+        
+        survey_dates = list(selected_survey["date_surveyed"].unique())
+        number_of_surveys = len(survey_dates)
+        
+        if measurements["date"].min() < min(survey_dates):
+            warnings.warn("Warning: There are data that precede the survey dates for: " + selected_site)
+            
+        if number_of_surveys == 1:
+            selected_measurements["date_surveyed"] = pd.to_datetime(np.where(selected_measurements["date"] >= survey_dates[0], survey_dates[0], np.nan))
+            
+        if number_of_surveys > 1:
+            survey_dates.append(pd.to_datetime(datetime.datetime.utcnow(), utc=True))
+            selected_measurements["date_surveyed"] = pd.to_datetime(pd.cut(selected_measurements["date"], bins = survey_dates, labels = survey_dates[:-1]), utc = True)
+    
+        merged_measurements_and_surveys = pd.merge(selected_measurements, surveys, how = "left", on = ["place","sensor_ID","date_surveyed"])
+        
+        matched_measurements = pd.concat([matched_measurements, merged_measurements_and_surveys]).drop_duplicates()
+        matched_measurements["notes"] = matched_measurements["notes_x"]
+        matched_measurements.drop(columns = ['notes_x','notes_y'],inplace=True)
+        
+    return matched_measurements
 
 
 def calc_baseline_wl(x, surveys):
@@ -135,6 +176,20 @@ def correct_drift(x, start_date, end_date):
     filtered_x = filtered_x.loc[:,["place", "sensor_ID", "date", "voltage", "sensor_water_depth", "qa_qc_flag", "date_surveyed", "sensor_elevation", "road_elevation", "lat", "lng", "alert_threshold", "min_water_depth", "deriv", "change_pt", "smoothed_min_water_depth", "sensor_water_level", "road_water_level", "sensor_water_level_adj", "road_water_level_adj"]]
 
     return filtered_x.set_index(["place", "sensor_ID", "date"])
+
+
+def postgres_upsert(table, conn, keys, data_iter):
+    from sqlalchemy.dialects.postgresql import insert
+
+    data = [dict(zip(keys, row)) for row in data_iter]
+
+    insert_statement = insert(table.table).values(data)
+    upsert_statement = insert_statement.on_conflict_do_update(
+        constraint=f"{table.table.name}_pkey",
+        set_={c.key: c for c in insert_statement.excluded},
+    )
+    conn.execute(upsert_statement)
+    
 
 def main():
 
